ccecc5c48548d4061b2284841c4b47238850a0dd
Alizado <164001577+Alizado@users.noreply.github.com>
2024-06-01 10:25:17 -0300

    Add files via upload

--- newfile.cxx
+++ newfile.cxx
@@ -1,27 +1,20 @@
 #include <iostream>
-
-int calcularPrecoIngresso(int idade) {
-    if (idade <= 17) {
-        return 15;
-    } else if (idade >= 60) {
-        return 20;
-    } else {
-        return 30;
-    }
-}
+#include <algorithm>
 
 int main() {
-    int idade1, idade2;
-
-    std::cout << "Digite a idade da primeira amiga: ";
+    int idade1, idade2, idade3;
+    
     std::cin >> idade1;
-
-    std::cout << "Digite a idade da segunda amiga: ";
     std::cin >> idade2;
-
-    int total = calcularPrecoIngresso(idade1) + calcularPrecoIngresso(idade2);
-
-    std::cout << "O total a ser pago pelos dois ingressos é: " << total << " Reais" << std::endl;
-
+    std::cin >> idade3;
+    
+    int idades[3] = {idade1, idade2, idade3};
+    
+    std::sort(idades, idades + 3);
+    
+    int idadeCamila = idades[1];
+    
+    std::cout << idadeCamila << std::endl;
+    
     return 0;
 }
